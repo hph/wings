b865a0f97d6bc8e9eca36f42b1fb3183bbaf98eb
Haukur Páll Hallvarðsson <hph@hph.is>
2017-06-09 01:05:42 +0100

    Add option to show or hide the line numbers

--- src/ui/components/view/index.js
+++ src/ui/components/view/index.js
@@ -1,9 +1,12 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
 
 import { Cursor } from 'ui/components';
 import css from './styles.css';
+
+const cx = classnames.bind(css);
 
 class View extends Component {
   static propTypes = {
@@ -13,9 +16,10 @@ class View extends Component {
 
   render () {
     const { config, view } = this.props;
+    const numbersClasses = cx('numbers', { hidden: !config.showLineNumbers });
     return (
       <div className={css.root}>
-        <div className={css.numbers}>
+        <div className={numbersClasses}>
           {_.map(view.lines, (line, index) => (
             <div key={index}>{index + 1}</div>
           ))}
