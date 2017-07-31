8201bf7885bcc37fafcc18513103638c2d70c815
Haukur Páll Hallvarðsson <hph@hph.is>
2017-07-31 01:26:25 +0100

    Split main process window logic into a separate module

--- src/main-process/index.js
+++ src/main-process/index.js
@@ -1,55 +1,7 @@
-import _ from 'lodash';
-import url from 'url';
-import path from 'path';
-import { BrowserWindow, app, ipcMain } from 'electron';
+import { app } from 'electron';
 
 import getConfig from 'main-process/config';
-
-const isAsar = process.mainModule.filename.indexOf('app.asar') > -1;
-
-function createWindow (mainWindow, config) {
-  mainWindow = new BrowserWindow({ // eslint-disable-line no-param-reassign
-    ...config.window,
-    backgroundColor: config.editor.theme.primaryBackgroundColor,
-    titleBarStyle: 'hidden',
-    show: false,
-  });
-
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true,
-    hash: JSON.stringify({
-      config: config.editor,
-      filename: process.argv[isAsar ? 1 : 2],
-    }),
-  }));
-
-  const show = _.once(() => {
-    mainWindow.show();
-    if (process.env.NODE_ENV === 'development') {
-      mainWindow.webContents.openDevTools({ detach: true });
-    }
-  });
-
-  let readyCount = 0;
-  const maybeShow = () => {
-    readyCount += 1;
-    if (readyCount >= 2) {
-      show();
-    }
-  };
-
-  // Show the app once the window is ready and the app has been mounted
-  // or after 1 second, whichever comes first.
-  mainWindow.once('ready-to-show', maybeShow);
-  ipcMain.once('root-mounted', maybeShow);
-  setTimeout(show, 1000);
-
-  mainWindow.once('closed', () => {
-    mainWindow = null; // eslint-disable-line no-param-reassign
-  });
-}
+import createWindow from 'main-process/main-window';
 
 getConfig()
   .then((config) => {
---
+++ src/main-process/main-window.js
@@ -0,0 +1,50 @@
+import _ from 'lodash';
+import url from 'url';
+import path from 'path';
+import { BrowserWindow, ipcMain } from 'electron';
+
+const isAsar = process.mainModule.filename.indexOf('app.asar') > -1;
+
+export default function createWindow (mainWindow, config) {
+  mainWindow = new BrowserWindow({ // eslint-disable-line no-param-reassign
+    ...config.window,
+    backgroundColor: config.editor.theme.primaryBackgroundColor,
+    titleBarStyle: 'hidden',
+    show: false,
+  });
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+    hash: JSON.stringify({
+      config: config.editor,
+      filename: process.argv[isAsar ? 1 : 2],
+    }),
+  }));
+
+  const show = _.once(() => {
+    mainWindow.show();
+    if (process.env.NODE_ENV === 'development') {
+      mainWindow.webContents.openDevTools({ detach: true });
+    }
+  });
+
+  let readyCount = 0;
+  const maybeShow = () => {
+    readyCount += 1;
+    if (readyCount >= 2) {
+      show();
+    }
+  };
+
+  // Show the app once the window is ready and the app has been mounted
+  // or after the configured timeout, whichever comes first.
+  mainWindow.once('ready-to-show', maybeShow);
+  ipcMain.once('root-mounted', maybeShow);
+  setTimeout(show, config.showTimeoutMs);
+
+  mainWindow.once('closed', () => {
+    mainWindow = null; // eslint-disable-line no-param-reassign
+  });
+}
