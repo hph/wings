7e80ede408e1296a4c3291216e0f6d12b4e04185
Haukur Páll Hallvarðsson <hph@hph.is>
2017-08-10 01:00:06 +0100

    Make TitleBar a PureComponent for a small gain
    This does not make much of a difference, but in this case it is faster
    to only rerender when the label changes.

--- src/ui/components/title-bar/index.js
+++ src/ui/components/title-bar/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import css from './styles.css';
 
-function TitleBar ({ label }) {
-  return <div className={css.root}>{label}</div>;
-}
+class TitleBar extends PureComponent {
+  static propTypes = {
+    label: PropTypes.string.isRequired,
+  };
 
-TitleBar.propTypes = {
-  label: PropTypes.string.isRequired,
-};
+  render () {
+    return <div className={css.root}>{this.props.label}</div>;
+  }
+}
 
 export default TitleBar;
