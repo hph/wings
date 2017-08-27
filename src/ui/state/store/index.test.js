83be151b4ca045b9bff579f7a229758f62bd5871
Haukur Páll Hallvarðsson <hph@hph.is>
2017-08-27 22:08:08 +0100

    Remove test that was breaking dev environment

--- src/ui/state/store/index.test.js
+++
@@ -1,7 +0,0 @@
-describe('store index', () => {
-  it('should import the production file when in production mode', () => {
-    process.env.NODE_ENV = 'production';
-    const configureStore = require('./index'); // eslint-disable-line global-require
-    expect(configureStore).toBeDefined();
-  });
-});
