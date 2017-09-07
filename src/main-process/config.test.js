a786f81aa7d9f07a79d4f3a045a3a96a1a79451b
Haukur Páll Hallvarðsson <hph@hph.is>
2017-09-07 20:43:44 +0100

    Modify fs-dependent test to run differently on Travis CI

--- src/main-process/config.test.js
+++ src/main-process/config.test.js
@@ -8,28 +8,32 @@ const tmp = tmpdir();
 const userConfigPath = path.join(homedir(), '.wings.conf.yaml');
 const tmpUserConfigPath = `${ tmp }/wings-conf`;
 
-beforeAll(done => {
-  fs.rename(userConfigPath, tmpUserConfigPath, done);
-});
-
-afterAll(done => {
-  fs.rename(tmpUserConfigPath, userConfigPath, done);
-});
-
-test('getConfig returns the default config if there is no user config', () => {
-  return getConfig().then(config => {
-    expect(config.isDefault).toBe(true);
-    expect(config.window).toBeDefined();
-    expect(config.editor).toBeDefined();
-  });
-});
-
-test('getConfig returns user-defined config values and falls back to defaults', () => {
-  return fs.writeFile(userConfigPath, 'window:\n  width: test-value')
-    .then(getConfig)
-    .then(config => {
-      expect(config.isDefault).toBe(false);
-      expect(config.window.width).toBe('test-value');
-      expect(config.window.height).toBe(600);
+describe('getConfig', () => {
+  if (!process.env.CI) {
+    beforeAll(done => {
+      fs.rename(userConfigPath, tmpUserConfigPath, done);
     });
+
+    afterAll(done => {
+      fs.rename(tmpUserConfigPath, userConfigPath, done);
+    });
+  }
+
+  it('returns the default config if there is no user config', () => {
+    return getConfig().then(config => {
+      expect(config.isDefault).toBe(true);
+      expect(config.window).toBeDefined();
+      expect(config.editor).toBeDefined();
+    });
+  });
+
+  it('returns user-defined config values and falls back to defaults', () => {
+    return fs.writeFile(userConfigPath, 'window:\n  width: test-value')
+      .then(getConfig)
+      .then(config => {
+        expect(config.isDefault).toBe(false);
+        expect(config.window.width).toBe('test-value');
+        expect(config.window.height).toBe(600);
+      });
+  });
 });
