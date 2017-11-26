dd28f5188ad37b1c389f9b7bde04ea8c0225885a
Haukur Páll Hallvarðsson <hph@hph.is>
2017-11-26 17:36:22 +0000

    Lock default config JSON file with basic tests

---
+++ src/main-process/default-config.test.js
@@ -0,0 +1,23 @@
+const defaultConfig = require('./default-config');
+
+describe('default config', () => {
+  it('should have window as a top-level key', () => {
+    expect(defaultConfig.window).toBeDefined();
+  });
+
+  it('should have editor as a top-level key', () => {
+    expect(defaultConfig.window).toBeDefined();
+  });
+
+  it('should only have two top-level keys', () => {
+    expect(Object.keys(defaultConfig).length).toBe(2);
+  });
+
+  it('should have a specific value for the window key', () => {
+    expect(defaultConfig.window).toMatchSnapshot();
+  });
+
+  it('should have a specific value for the editor key', () => {
+    expect(defaultConfig.editor).toMatchSnapshot();
+  });
+});
