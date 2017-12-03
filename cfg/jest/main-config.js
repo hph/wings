2340d07314481474361d390f83baa5abff778329
Haukur Páll Hallvarðsson <hph@hph.is>
2017-12-03 15:30:04 +0000

    Rename config to cfg

---
+++ cfg/jest/main-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  projects: [
+    '<rootDir>/cfg/jest/eslint-config.js',
+    '<rootDir>/cfg/jest/prettier-config.js',
+    '<rootDir>/cfg/jest/test-config.js',
+  ],
+};
--- config/jest/main-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  projects: [
-    '<rootDir>/config/jest/eslint-config.js',
-    '<rootDir>/config/jest/prettier-config.js',
-    '<rootDir>/config/jest/test-config.js',
-  ],
-};
